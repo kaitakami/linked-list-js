1f0fdd72abb551960d77b7c9b001ed2719119876
Kai Takami <kaitakami@protonmail.com>
2022-08-16 11:09:59 +0900

    Finish linked list factory function, TOP project

--- linked-list.js
+++ linked-list.js
@@ -1,89 +1,157 @@
 class Node {
   constructor(value) {
-    this.value = value,
-      this.next = null
+    (this.value = value), (this.next = null);
   }
 }
 
 function LinkedList() {
-  let head = null
+  let head = null;
   function append(value) {
-    let newNode = new Node(value)
+    let newNode = new Node(value);
     let currentNode;
     if (head === null) {
-      head = newNode
+      head = newNode;
     } else {
-      currentNode = head
+      currentNode = head;
       while (currentNode.next) {
-        currentNode = currentNode.next
+        currentNode = currentNode.next;
       }
-      currentNode.next = newNode
+      currentNode.next = newNode;
     }
-    return head // for testing, not necessary
+    return head; // for testing, not necessary
   }
 
   function prepend(value) {
-    let newNode = new Node(value)
+    let newNode = new Node(value);
     let currentNode;
     if (head === null) {
-      head = newNode
+      head = newNode;
     } else {
-      newNode.next = head
-      head = newNode
+      newNode.next = head;
+      head = newNode;
     }
-    return head // for testing, not necessary
+    return head; // for testing, not necessary
   }
 
-  function size () {
-    let counter = 1
-    let nodeTraversal = head
+  function size() {
+    let counter = 1;
+    let nodeTraversal = head;
     while (nodeTraversal.next) {
-      nodeTraversal = nodeTraversal.next
-      counter++
+      nodeTraversal = nodeTraversal.next;
+      counter++;
     }
-    return counter
+    return counter;
   }
 
-  function returnHead () {
-    return head
+  function returnHead() {
+    return head;
   }
 
-  function returnTail () {
+  function returnTail() {
     let tail;
-    let list = head
+    let list = head;
     while (list.next) {
       if (list.next.next === null) {
-        tail = list.next
-        list = list.next
+        tail = list.next;
+        list = list.next;
       } else {
-        list = list.next
+        list = list.next;
       }
     }
-    return tail
+    return tail;
   }
 
   function indexAt(index) {
     let indexElement;
-    let list = head
-    let indexCount = 0 // Starting from zero
+    let list = head;
+    let indexCount = 0; // Starting from zero
     while (indexCount <= index) {
       if (indexCount === index) {
-        indexElement = list 
+        indexElement = list;
       }
-      list = list.next
-      indexCount++
+      list = list.next;
+      indexCount++;
     }
-    return indexElement
+    return indexElement;
   }
 
-  return { append, prepend, size, returnHead, returnTail, indexAt }
+  function pop() {
+    let list = head;
+    while (list.next) {
+      if (list.next.next === null) {
+        list.next = null;
+      } else {
+        list = list.next;
+      }
+    }
+    return head;
+  }
 
+  function contains(value) {
+    let valueExist = false;
+    let list = head;
+    while (list.next) {
+      if (list.value === value) {
+        valueExist = true;
+      }
+      list = list.next;
+    }
+    return valueExist;
+  }
+
+  function find(value) {
+    let index = 0;
+    let valueIndex = null;
+    let list = head;
+    while (list.next) {
+      if (list.value === value) {
+        valueIndex = index;
+      }
+      index++;
+      list = list.next;
+    }
+    if (!valueIndex) {
+      let tail = returnTail();
+      if (tail.value === value) {
+        valueIndex = index;
+      }
+    }
+    return valueIndex;
+  }
+
+  function toString() {
+    if (head === null) {
+      return null;
+    }
+
+    let listStr = "";
+    let list = head;
+    while (list.next) {
+      if (listStr === "") {
+        listStr += `(${list.value})`;
+      } else {
+        listStr += ` -> (${list.value})`;
+      }
+      list = list.next;
+    }
+
+    let tail = returnTail();
+    listStr += ` -> (${tail.value})`;
+    return listStr;
+  }
+
+  return {
+    append,
+    prepend,
+    size,
+    returnHead,
+    returnTail,
+    indexAt,
+    pop,
+    contains,
+    find,
+    toString,
+  };
 }
 
-/*const testList = LinkedList()
-testList.prepend(4)
-testList.prepend(3)
-testList.prepend(2)
-console.log(testList.prepend(1)) */
-
-export { Node, LinkedList }
+export { Node, LinkedList };
--- linked-list.test.js
+++ linked-list.test.js
@@ -46,8 +46,8 @@ test("prepend and append function", () => {
           value: 3,
           next: {
             value: 4,
-            next: null
-          }
+            next: null,
+          },
         },
       },
     });
@@ -58,59 +58,145 @@ test("size function", () => {
   testList.prepend(2),
     testList.append(3),
     testList.prepend(1),
-    testList.append(4)
+    testList.append(4);
   expect(testList.size()).toEqual(4);
-})
+});
 
 test("head function", () => {
   const testList = LinkedList();
-  testList.append(1),
-    testList.append(2),
-    testList.append(3)
+  testList.append(1), testList.append(2), testList.append(3);
   expect(testList.returnHead()).toEqual({
     value: 1,
     next: {
       value: 2,
       next: {
         value: 3,
-        next: null
-      }
-    }
+        next: null,
+      },
+    },
   });
-})
+});
 
 test("tail function", () => {
   const testList = LinkedList();
-  testList.append(1),
-    testList.append(2),
-    testList.append(3)
+  testList.append(1), testList.append(2), testList.append(3);
   expect(testList.returnTail()).toEqual({
-      value: 3,
-      next: null
+    value: 3,
+    next: null,
   });
-})
+});
 
 test("indexAt function", () => {
   const testList = LinkedList();
-  testList.append(1),
-    testList.append(2),
-    testList.append(3)
+  testList.append(1), testList.append(2), testList.append(3);
   expect(testList.indexAt(1)).toEqual({
-      value: 2,
-      next: {
-        value: 3,
-        next: null
-      }
+    value: 2,
+    next: {
+      value: 3,
+      next: null,
+    },
   });
-})
+});
 
 test("indexAt function in last node", () => {
   const testList = LinkedList();
+  testList.append(1), testList.append(2), testList.append(3);
+  expect(testList.indexAt(2)).toEqual({
+    value: 3,
+    next: null,
+  });
+});
+
+test("pop function", () => {
+  const testList = LinkedList();
   testList.append(1),
     testList.append(2),
-    testList.append(3)
-  expect(testList.indexAt(2)).toEqual({
-      value: 3,
-      next: null
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.pop()).toEqual({
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: {
+            value: 5,
+            next: null,
+          },
+        },
+      },
+    },
   });
-})
+});
+
+test("contains function return true", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.contains(3)).toBe(true);
+});
+
+test("contains function return false", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.contains(7)).toBe(false);
+});
+
+test("find function return null", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.find(7)).toBe(null);
+});
+
+test("find function return index", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.find(3)).toBe(2);
+});
+
+test("find function return index of last value", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    testList.append(4),
+    testList.append(5),
+    testList.append(6);
+  expect(testList.find(6)).toBe(5);
+});
+
+test("toString function", () => {
+  const testList = LinkedList();
+  testList.append(1),
+    testList.append(2),
+    testList.append(3),
+    expect(testList.toString()).toBe("(1) -> (2) -> (3)");
+});
+
+test("toString no nodes", () => {
+  const testList = LinkedList();
+  expect(testList.toString()).toBe(null);
+});
